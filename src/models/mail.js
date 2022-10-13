78d0efcf032efd38981b83703fe54903ec7fb558
Anna Berg <anbj21@student.bth.se>
2022-10-13 15:20:50 +0200

    10.0.1

--- src/models/mail.js
+++ src/models/mail.js
@@ -2,7 +2,8 @@ const baseURL = "https://jsramverk-editor-anbj21.azurewebsites.net";
 
 const mail = {
     mailer: async function mailer(code, title, name, mottagare) {
-        await fetch(`${baseURL}/mail/${code}/${title}/${name}/${mottagare}`);
+        const result = await fetch(`${baseURL}/mail/${code}/${title}/${name}/${mottagare}`);
+        console.log(result)
         return "Email sent!";
     }
 };
